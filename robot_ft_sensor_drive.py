Bug fix: Vector rotation was in the wrong direction

robot_ft_sensor_drive.py
@@ -23,7 +23,7 @@ def follow_state_sequence() -> None:
     ur10.move_home()
 
     # Create a monitor to display ft readings
-    ft_monitor = SignalMonitor(AX_LABELS_, round(1 / ur10.ft_sensor.time_step), 10.0)
+    ft_monitor = SignalMonitor(AX_LABELS_, round(1 / ur10.ft_sensor.time_step), 3.14)
     sub_steps = int(ft_monitor.display_rate / ur10.ft_sensor.time_step)
     LOGGER.info(f'Perform {sub_steps} sub-steps before updating the Monitor.')
 
@@ -34,9 +34,9 @@ def follow_state_sequence() -> None:
         t_start_ = time.time()
         t_next_ = t_start_
         while True:
-            ft_signal = np.reshape(ur10.get_tcp_force(extern=False).xyzXYZ, [6, 1])
+            ft_signal = np.reshape(ur10.get_tcp_force(extern=True).xyzXYZ, [6, 1])
             for i in range(sub_steps - 1):
-                ft_signal = np.hstack([ft_signal, np.reshape(ur10.get_tcp_force(extern=False).xyzXYZ, [6, 1])])
+                ft_signal = np.hstack([ft_signal, np.reshape(ur10.get_tcp_force(extern=True).xyzXYZ, [6, 1])])
                 time.sleep(ur10.ft_sensor.time_step)
 
             t_next_ += ft_monitor.display_rate

ur_pilot/core.py
@@ -233,7 +233,7 @@ class URPilot:
             tcp_force = Vector6d().from_xyzXYZ(self.rtde.r.getActualTCPForce())
         # Compensate Tool mass
         f_tool_wrt_world = self.tool.f_inertia
-        f_tool_wrt_ft = (self.q_world2arm * self.get_tcp_pose().q).apply(f_tool_wrt_world)
-        t_tool_wrt_ft = Vector3d().from_xyz(np.cross(f_tool_wrt_ft.xyz, self.tool.com.xyz))
+        f_tool_wrt_ft = (self.q_world2arm * self.get_tcp_pose().q).apply(f_tool_wrt_world, inverse=True)
+        t_tool_wrt_ft = Vector3d().from_xyz(np.cross(self.tool.com.xyz, f_tool_wrt_ft.xyz))
         ft_comp = Vector6d().from_Vector3d(f_tool_wrt_ft, t_tool_wrt_ft)
         return tcp_force + ft_comp