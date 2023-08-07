ba1585d5c4063036fca94cc3b6688febc0908715
Georg John <georg.john@dfki.de>
2023-08-07 13:00:25 +0200

    Add calculation to get the new tool offset

--- robot_measure_tool_length.py
+++ robot_measure_tool_length.py
@@ -4,7 +4,7 @@ import logging
 import argparse
 import ur_pilot
 import numpy as np
-from rigmopy import Pose
+from rigmopy import Pose, Vector3d, Quaternion
 
 LOGGER = logging.getLogger(__name__)
 
@@ -16,6 +16,9 @@ START_POSE_TILTED = Pose().from_xyz([0.469, 0.202, 0.077]).from_axis_angle([0.0,
 
 # Tilted pose = Pose(xyz=(0.5083401135463013, 0.20196865604992476, 0.1625605107788487), wxyz=(0.13055575770270553, -3.613938400232128e-05, -0.9914409678850301, 4.879763870700817e-06))
 # Straight pose = Pose(xyz=(0.4689866250748727, 0.20201923372461283, 0.1633334585259315), wxyz=(3.762129487057298e-05, 8.89461762450254e-06, -0.9999999991896941, -1.1231022160377069e-05))
+
+# tilted_offset = Vector3d(-0.025, 0.0, 0.15122), (0.0, -0.2618, 0.0)
+
 
 def main() -> None:
 
@@ -59,6 +62,16 @@ def main() -> None:
             # Move back to home position()
             pilot.move_home()
 
+        # Calculate tilted tool offset.
+        tcp_offset_straight = pilot.robot.get_tcp_offset()
+        # We are only interested in Z - direction
+        z_flange_tilted = contact_flange_pose_tilted.xyz[-1]
+        z_flange_straight = contact_flange_pose_straight.xyz[-1]
+        z_diff = z_flange_straight - z_flange_tilted
+        tcp_offset_tilted = Pose().from_pq(p=tcp_offset_straight.p - Vector3d().from_xyz([-0.025, 0.0, z_diff]))
+        tcp_offset_tilted = tcp_offset_tilted.from_pq(q=tcp_offset_straight.q * Quaternion().from_axis_angle([0.0, -rot_15_deg, 0.0])) 
+
+        LOGGER.info(f"Find new offset for tilted tool with pose: {tcp_offset_tilted.p, tcp_offset_tilted.axis_angle}")
 
 if __name__ == '__main__':
     """ Script to measure the tool length in a odd orientation """
