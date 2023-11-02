ca3fef667af13456d22a3cbf137f5c1d7466f61f
Georg John <georg.john@dfki.de>
2023-11-02 12:38:10 +0100

    Add action

--- examples/robot_connect_to_socket.py
+++ examples/robot_connect_to_socket.py
@@ -96,7 +96,8 @@ def connect_to_socket() -> None:
                 pilot.move_to_tcp_pose(T_Base2SocketPre.pose)
             time.sleep(1.0)
             with pilot.force_control():
-                fin = pilot.plug_in_force_ramp(f_axis='z', f_start=50.0, f_end=110, duration=6.0)
+                fin, _ = pilot.find_contact_point(direction=[0, 0, 1, 0, 0, 0], time_out=5.0)
+                fin = pilot.plug_in_force_ramp(f_axis='z', f_start=75.0, f_end=125, duration=4.0)
                 print(f"Finished early: {fin}")
                 pilot.relax(3.0)
                 # Try to plug out
