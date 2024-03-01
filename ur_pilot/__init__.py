22eeb49734ac2250d240be5de5371fb1d5e63b02
Georg John <georg.john@dfki.de>
2024-03-01 14:34:35 +0100

    Add demo script

--- ur_pilot/__init__.py
+++ ur_pilot/__init__.py
@@ -3,7 +3,7 @@ from ur_pilot.core import Pilot
 from ur_pilot.core import connect
 import ur_pilot.base_logger as logger
 import ur_pilot.config.utilities.configs as config_models
-import ur_pilot.config.yaml_helpers as yaml_helpers
+import ur_pilot.config.utilities.yaml_helpers as yaml_helpers
 from ur_pilot.monitor.signal_monitor import SignalMonitor
 from ur_pilot.end_effector.hand_eye_calibration import HandEyeCalibration
 from ur_pilot.end_effector.flange_eye_calibration import FlangeEyeCalibration
