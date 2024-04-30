c316dbae20cc7d3756ae292dfe9552125b5ccb99
Gurunatraj <gurunatraj.parthasarathy@dfki.de>
2024-04-30 17:11:24 +0200

    Update demo

--- scripts/robot_free_drive.py
+++ scripts/robot_free_drive.py
@@ -15,16 +15,18 @@ def free_drive() -> None:
     with ur_pilot.connect(config_data.robot_dir) as pilot:
         # Start free drive mode
         LOGGER.info("Hit key 'S' to print out the current state")
-        with pilot.context.teach_in_control():
-            while not ck.user.stop():
-                if ck.user.save():
-                    # Read joint and pose information
-                    joint_pos = pilot.robot.joint_pos
-                    tcp_pose = pilot.robot.tcp_pose
-                    # Print out
-                    LOGGER.info(f"        Joint positions: {ur_pilot.utils.vec_to_str(joint_pos)}")
-                    LOGGER.info(f"Transformation Base-TCP: {ur_pilot.utils.se3_to_str(tcp_pose)}")
-                cam.render()
+        with pilot.plug_model.context('type2_female'):
+            with pilot.context.teach_in_control():
+                while not ck.user.stop():
+                    if ck.user.save():
+                        # Read joint and pose information
+                        joint_pos = pilot.robot.joint_pos
+                        # tcp_pose = pilot.robot.tcp_pose
+                        tcp_pose = pilot.get_pose(ur_pilot.EndEffectorFrames.PLUG_LIP)
+                        # Print out
+                        LOGGER.info(f"        Joint positions: {ur_pilot.utils.vec_to_str(joint_pos)}")
+                        LOGGER.info(f"Transformation Base-TCP: {ur_pilot.utils.se3_to_str(tcp_pose)}")
+                    cam.render()
     cam.end()
 
 
